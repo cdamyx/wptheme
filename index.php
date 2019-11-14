2751db06753e4040b27fd505559f7f01a819c734
cdamyx <chris@amyx.engineering>
2019-11-13 18:34:22 -0600

    testing with functions

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
-<h1>test</h1>
-
 <?php
+	function myFunc($name, $color) {
+		echo "<p>Hello my name is $name and my favorite color is $color</p>";
+	}
 
-	echo "hello world";
-
+	myFunc('Jack', 'Black');
 ?>
 
-<h2>test2</h2>
-<h3>test3</h3>
-<h4>test4</h4>
-<h5>test5</h5>
-<h6>test6</h6>
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
